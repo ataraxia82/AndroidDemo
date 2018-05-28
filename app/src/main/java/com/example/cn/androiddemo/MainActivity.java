546df8f42c30b33378ec444421862f48d826d34d
ataraxia82 <jjbzxmft@163.com>
2018-05-28 13:36:14 +0800

    selector选择器 & listview

--- app/src/main/java/com/example/cn/androiddemo/MainActivity.java
+++ app/src/main/java/com/example/cn/androiddemo/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.cn.androiddemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 事件处理
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) this.findViewById(R.id.listdata);
+
     }
 }
