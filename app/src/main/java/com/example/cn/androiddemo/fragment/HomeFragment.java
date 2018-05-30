f27d3718c4c24e64f0f57536fd254ae6054a1a86
ataraxia82 <jjbzxmft@163.com>
2018-05-30 16:32:53 +0800

    fragment with webview

--- app/src/main/java/com/example/cn/androiddemo/fragment/HomeFragment.java
+++ app/src/main/java/com/example/cn/androiddemo/fragment/HomeFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.example.cn.androiddemo.R;
 
@@ -14,6 +17,10 @@ import com.example.cn.androiddemo.R;
  */
 public class HomeFragment extends Fragment {
 
+    private WebView webView;
+    // 需要加载的网页URL地址
+    private String url=
+            "https://www.baidu.com";
 
     public HomeFragment() {
         // Required empty public constructor
@@ -24,7 +31,35 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view=inflater.inflate(R.layout.fragment_home, container, false);
+        webView = (WebView) view.findViewById(R.id.wv);
+
+        initWebView();
+        return view;
     }
+
+    private void initWebView() {
+        webView.setWebViewClient(new WebViewClient() {
+                    @Override
+                    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                        view.loadUrl(url);
+                        return super.shouldOverrideUrlLoading(view, url);
+                    }
+                }
+        );
+
+        webView.loadUrl(url);
+        // 得到setting
+        WebSettings webSettings=webView.getSettings();
+        //设置支持Javascript
+        webSettings.setJavaScriptEnabled(true);
+        //支持的语言类型
+        webSettings.setDefaultTextEncodingName("UTF-8");
+        //设置可以访问文件
+        webSettings.setAllowFileAccess(true);
+
+        webSettings.setDomStorageEnabled(true);
+    }
+
 
 }
